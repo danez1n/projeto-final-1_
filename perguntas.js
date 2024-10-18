ea45734766358bc20141272b36f8721f3251a33e
danez1n <166774208+danez1n@users.noreply.github.com>
2024-10-17 21:59:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'O que foi a Revolução Industrial?',
+    'A Revolução Industrial foi um período de grandes mudanças econômicas e sociais que começou no final do século XVIII, caracterizado pela transição de uma economia agrária para uma economia industrial, com o uso de máquinas e fábricas.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é um número natural maior que 1 que só pode ser dividido por 1 e por ele mesmo.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é a célula?',
+    'A célula é a unidade básica da vida, responsável por todas as funções vitais dos organismos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'O maior país do mundo é a Rússia.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um substantivo?',
+    'Substantivo é a palavra que nomeia pessoas, lugares, coisas ou ideias. '
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável é um espaço na memória que armazena um valor que pode ser alterado durante a execução de um programa.'
+)
